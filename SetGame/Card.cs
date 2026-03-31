f1232d7a7860cda937a21113f4bd0afa12e89234
pelreshef@gmail.com <pelreshef@gmail.com>
2026-03-31 03:50:12 +0300

    can draw card objects on the board

---
+++ SetGame/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetGame
+{
+    public class Card
+    {
+        Shape shape;
+        Color color;
+        Fill fill;
+        Count count;
+
+        public Card(Shape shape, Color color, Fill fill, Count count)
+        {
+            this.shape = shape;
+            this.color = color;
+            this.fill = fill;
+            this.count = count;
+        }
+
+        public Shape GetShape() { return shape; }
+        public Color getColor() { return color; }
+        public Fill getFill() { return fill; }
+        public Count getCount() { return count; }
+    }
+    public enum Shape
+    {
+        Squiggle,
+        Diamond,
+        Capsule
+    }
+    public enum Color
+    {
+        Green,
+        Purple,
+        Red
+    }
+    public enum Fill
+    {
+        Full,
+        Hollow,
+        Striped
+    }
+    public enum Count
+    {
+        One,
+        Two, 
+        Three
+    }
+}
--- SetGame/MainWindow.xaml.cs
+++ SetGame/MainWindow.xaml.cs
@@ -23,6 +23,85 @@ namespace SetGame
         public MainWindow()
         {
             InitializeComponent();
+            Card card = new Card(Shape.Capsule, Color.Green, Fill.Striped, Count.Three);
+            DisplayCard(card, 2, 1);
+        }
+
+        public void DisplayCard(Card card, int x, int y)
+        {
+            Border border = new Border()
+            {
+                BorderThickness = new Thickness(2),
+                Margin = new Thickness(15),
+                BorderBrush = new SolidColorBrush(Colors.Black),
+            };
+            Viewbox vb = new Viewbox()
+            {
+                Margin = new Thickness(2),
+            };
+            StackPanel sp = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            for (int i = 0; i < (int)card.getCount() + 1; i++)
+            {
+                Path p = new Path()
+                {
+                    Margin = new Thickness(2),
+                    Stretch = Stretch.Uniform,
+                    StrokeThickness = 1
+                };
+                
+                p.Data = (Geometry)this.Resources[card.GetShape().ToString()];
+
+               var color = Colors.White; // will get overriden
+
+                switch (card.getColor())
+                {
+                    case Color.Red:
+                        color = Colors.Red;
+                        break;
+                    case Color.Green:
+                        color = Colors.Green;
+                        break;
+                    case Color.Purple:
+                        color = Colors.Purple;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                p.Stroke = new SolidColorBrush(color);
+                int opacity = 0;
+                switch (card.getFill())
+                {
+                    case Fill.Full:
+                        opacity = 255;
+                        break;
+                    case Fill.Striped:
+                        opacity = 64;
+                        break;
+                    case Fill.Hollow:
+                        opacity = 0;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                color.A = (byte)opacity;
+                p.Fill = new SolidColorBrush(color);
+
+                
+
+                sp.Children.Add(p);
+            }
+            vb.Child = sp;
+            border.Child = vb;
+            Grid.SetColumn(border, x);
+            Grid.SetRow(border, y);
+            this.cardsGrid.Children.Add(border);
+            
         }
     }
 }
